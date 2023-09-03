a1a505fb4e183185f3bafe1283e93890a6b05197
jieye/mbp <jieye.ericx@gmail.com>
2023-09-03 17:46:45 +0800

    update
    Signed-off-by: jieye/mbp <jieye.ericx@gmail.com>

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "jieye の 数字花园",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jieye.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
